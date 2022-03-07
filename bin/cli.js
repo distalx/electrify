cab7febe34d4f43d884001974fb3a05ad84e3eda
distalx <distalx@gmail.com>
2022-03-07 17:00:11 +0530

    remove `electron-prebuilt` in favour of `electron`

--- bin/cli.js
+++ bin/cli.js
@@ -66,7 +66,7 @@ if(process.argv.length == 2 || -1 == 'run|bundle|package'.indexOf(cmd) ){
 function run_electron(){
   var input         = program.input || process.cwd();
   var electrify_dir = join(input, '.electrify');
-  var electron_path = require('electron-prebuilt');
+  var electron_path = require('electron');
   var settings      = parse_meteor_settings(true);
 
   if(settings)
@@ -114,7 +114,7 @@ function electrify(create) {
     console.error('input folder doesn\'t exist\n  ' + program.input);
     process.exit();
   }
-  
+
   input = program.input || process.cwd();
 
   if(!is_meteor_app()) {
@@ -138,7 +138,7 @@ function electrify(create) {
 
       // if its not installed, install it
       var pwd = shell.pwd();
-      
+
       shell.cd(input);
       shell.exec('meteor add arboleya:electrify');
       shell.cd(pwd);
@@ -161,7 +161,7 @@ function has_local_electrify(){
     console.error('input folder doesn\'t exist\n  ' + program.input);
     process.exit();
   }
-  
+
   var input = program.input || process.cwd();
 
   // validates meteor project
@@ -219,7 +219,7 @@ function parse_packager_options(){
       var parts = arg.split('=');
       var key = parts[0];
       var val = 'undefined' == typeof(parts[1]) ? true : parts[1];
-    
+
       if(~names.indexOf(key))
         options[key.slice(2)] = val;
       else
